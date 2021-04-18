2513da26d644c300ddc648570290b23652f7d2e1
ankitamaps03 <37062027+ankitamaps03@users.noreply.github.com>
2021-04-18 13:45:51 -0500

    Updated css of history helper

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ type HeaderProps ={
 const Header = ({name, icon}: HeaderProps) => {
     return (
         <div>
-            <IonIcon style={{width: "5%", float: "left", marginTop: "5px" , fontSize: "20px"}}
+            <IonIcon style={{width: "5%", float: "left", marginTop: "5px" , fontSize: "30px"}}
                      icon={icon}/>
             <h2>{name}</h2>
         </div>)
